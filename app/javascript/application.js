b1968170d89ef734926b52a1bfe02ace3c04c864
Pudding(Lloyd) <89757149+KiyoScript@users.noreply.github.com>
2024-08-16 17:19:04 +0800

    RD-SettingUp-Sneat (#2)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "perfect-scrollbar"
+import "template-customizer"
+import "menu"
+import "mega-dropdown"
+import "dropdown-hover"
+import "helpers"
+import "config"
+import "bootstrap"
+import "main"
+
