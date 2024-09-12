4de895a0714b340cde0db1bc34297a955935ad70
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-09-12 18:38:16 +0800

    RD-Set Disabled true for age field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if resource.incomplete?
+    if resource.incomplete? || resource.deactivated?
       onboarding_path(resource)
     else
       root_path
