499dbcb5d41435d7076737f5d7d7946cc4c9df47
DanTech2002 <danielligutan123@gmail.com>
2024-10-09 11:40:47 +0800

    RD - Strada Default Configuration

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,23 @@
 import { Application } from "@hotwired/stimulus"
+import "@hotwired/strada"
+
+import MenuController from "./menu_controller.js"
+import BridgeFormController from "./bridge/form_controller.js"
+import BridgeMenuController from "./bridge/menu_controller.js"
+import BridgeOverflowMenuController from "./bridge/overflow_menu_controller.js"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+// Register Controllers
+Stimulus.register("menu", MenuController)
+
+// Register Bridge Components
+Stimulus.register("bridge--form", BridgeFormController)
+Stimulus.register("bridge--menu", BridgeMenuController)
+Stimulus.register("bridge--overflow-menu", BridgeOverflowMenuController)
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/strada", to: "https://cdn.jsdelivr.net/npm/@hotwired/strada@latest/dist/strada.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "helpers"
 pin "config"
