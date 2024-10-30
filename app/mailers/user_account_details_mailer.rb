1b3357b7dcd0977d27f544b8c3a7c214789fdbd4
DanTech2002 <danielligutan123@gmail.com>
2024-10-30 15:07:51 +0800

    RD-Notifications-Email-Template

--- app/mailers/user_account_details_mailer.rb
+++ app/mailers/user_account_details_mailer.rb
@@ -2,6 +2,6 @@ class UserAccountDetailsMailer < ApplicationMailer
   def send_email(user, password)
     @user = user
     @password = password
-    mail(to: email_to_deliver('danielligutan123@gmail.com'), subject: "RentDito - Your account details!!!")
+    mail(to: email_to_deliver('danielligutan123@gmail.com'), subject: "Welcome to RentDito, #{@user.firstname}!")
   end
 end
