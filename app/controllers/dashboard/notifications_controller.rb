3b92c87ac6d9e869094a479ceb5cb74557d9e546
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-03 12:12:39 +0800

    RD-Added Notifications Page

--- app/controllers/dashboard/notifications_controller.rb
+++ app/controllers/dashboard/notifications_controller.rb
@@ -1,9 +1,21 @@
 class Dashboard::NotificationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_policy!, only: :index
+  def index
+    @q = current_user.notifications.ransack(params[:q])
+    @pagy, @notifications = pagy(@q.result.order(created_at: :desc), distinct: :true)
+  end
+
   def mark_as_read
     notification = current_user.notifications.find(params[:id])
     notification.update(read: true)
     head :ok
   end
 
+  private
+
+  def set_policy!
+    authorize User, policy_class: Dashboard::NotificationsPolicy
+  end
 
 end
