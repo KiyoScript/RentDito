e285aa8a67ce144b2990b8ddcac292acf2f8a6c0
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-04 18:44:14 +0800

    RD-Updated Manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,7 +3,7 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
-//= link controllers/menu_controller.js
-//= link controllers/bridge/form_controller.js
-//= link controllers/bridge/menu_controller.js
-//= link controllers/bridge/overflow_menu_controller.js
+//= link ../javascript/controllers/menu_controller.js
+//= link ../javascript/controllers/bridge/form_controller.js
+//= link ../javascript/controllers/bridge/menu_controller.js
+//= link ../javascript/controllers/bridge/overflow_menu_controller.js
