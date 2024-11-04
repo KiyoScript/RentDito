a8b898c6c1ae481c6ceb84d6d7028525e99e5106
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-04 16:44:48 +0800

    RD-HotFix Added Default URL Options for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,9 @@ Rails.application.configure do
   #
   config.action_mailer.perform_deliveries = true
 
+
+  config.action_mailer.default_url_options = { host: 'rentdito.fly.dev', protocol: 'https' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:                      'smtp.gmail.com',
