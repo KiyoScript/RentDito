8f9e3f1d37299ff33334a771b6627918dbf9e489
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-04 18:50:14 +0800

    RD-Removed Manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,8 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-
-//= link ../javascript/controllers/menu_controller.js
-//= link ../javascript/controllers/bridge/form_controller.js
-//= link ../javascript/controllers/bridge/menu_controller.js
-//= link ../javascript/controllers/bridge/overflow_menu_controller.js
