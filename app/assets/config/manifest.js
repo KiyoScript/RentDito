a6398137c8cd852f51920ce5c9cefe09dad5e95a
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-04 17:12:53 +0800

    RD-Added Controllers Bridge JS

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../javascript/bridge .js
+
+//= link controllers/menu_controller.js
+//= link controllers/bridge/form_controller.js
+//= link controllers/bridge/menu_controller.js
+//= link controllers/bridge/overflow_menu_controller.js
