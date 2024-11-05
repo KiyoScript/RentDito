04db7dc31de734cb62379b263463269f223844c1
DanTech2002 <danielligutan123@gmail.com>
2024-11-05 13:14:56 +0800

    RD-Restyle Images and Email Templates

--- app/mailers/reset_password_instructions_mailer.rb
+++ app/mailers/reset_password_instructions_mailer.rb
@@ -2,6 +2,6 @@ class ResetPasswordInstructionsMailer < ApplicationMailer
   def send_email(user, token, opts={})
     @user = user
     @token = token
-    mail(from: email_sender, to: email_to_deliver(user.email), subject: "Reset Password Instructions.")
+    mail(from: email_sender, to: email_to_deliver(user.email), subject: "Reset password instructions for #{@user.firstname}.")
   end
 end
