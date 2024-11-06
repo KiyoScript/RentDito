6501306917d626c46f13269b80133334df3f4678
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-06 09:00:40 +0800

    RD-HotFix Forgot Password Generate Token for the url can be accessible

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -13,8 +13,9 @@ class Users::PasswordsController < Devise::PasswordsController
   #
   def create
     resource = User.find_by(email: resource_params.dig(:email))
+    token = resource.send(:set_reset_password_token)
     if successfully_sent?(resource)
-      ResetPasswordInstructionsMailer.send_email(resource, resource.reset_password_token).deliver_now
+      ResetPasswordInstructionsMailer.send_email(resource, token).deliver_now
       respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
     else
       respond_with resource
