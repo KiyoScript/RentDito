6d804a687fd0d8b9c20283bdd9604b9ba826b291
DanTech2002 <danielligutan123@gmail.com>
2024-11-09 05:59:52 +0800

    RD-Updated Penalty Rate

--- app/services/charge_penalty_calculation.rb
+++ app/services/charge_penalty_calculation.rb
@@ -17,11 +17,13 @@ class ChargePenaltyCalculation
     return 0 if overdue_days <= 0  # No penalty if due date is not passed
 
     case overdue_days
-    when 1..5
-      0.05  # 5% penalty for 1-5 days overdue
-    when 6..Float::INFINITY
-      # Start at 5% and increase by 10% for each additional overdue day
-      0.05 + (overdue_days - 5) * 0.10
+    when 1
+      0.05  # 5% penalty for 1 days overdue
+    when 2..6
+      0.15  # 15% penalty for 2 to 6 days overdue
+    when 7..Float::INFINITY
+      # Start at 15% and increase by 10% for each additional overdue every 5 days
+      0.15 + ((overdue_days - 2) / 5) * 0.10
     end
   end
 end
