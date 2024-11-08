4747fc685b6278acdd233e546da7cfdd756eca3c
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-08 16:17:17 +0800

    RD-Admins can create accounts

--- app/policies/dashboard/utility_staff_policy.rb
+++ app/policies/dashboard/utility_staff_policy.rb
@@ -4,10 +4,10 @@ class Dashboard::UtilityStaffPolicy < ApplicationPolicy
   end
 
   def new?
-    user.landlord?
+    user.landlord? || (user.admin? && user.verified?)
   end
 
   def create?
-    user.landlord?
+    user.landlord? || (user.admin? && user.verified?)
   end
 end
