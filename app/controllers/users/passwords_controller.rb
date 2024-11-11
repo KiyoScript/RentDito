a5685971b3a084448425a57fcbb65194550676da
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-11 17:23:33 +0800

    RD-Forgot Password User Not Exist

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -13,12 +13,13 @@ class Users::PasswordsController < Devise::PasswordsController
   #
   def create
     resource = User.find_by(email: resource_params.dig(:email))
-    token = resource.send(:set_reset_password_token)
-    if successfully_sent?(resource)
-      ResetPasswordInstructionsMailer.send_email(resource, token).deliver_now
+    if resource
+      token = resource.send(:set_reset_password_token)
+      ResetPasswordInstructionsMailer.send_email(resource, token).deliver_now if successfully_sent?(resource)
       respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
     else
-      respond_with resource
+      flash[:alert] = "The email address you entered does not exist in our records."
+      redirect_to new_user_password_path
     end
   end
 
