824f477d6eb39cb3e9f2da28074013d6beecbe31
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-15 23:18:59 +0800

    RD-HotFIx City

--- app/models/maintenance_staff.rb
+++ app/models/maintenance_staff.rb
@@ -1,7 +1,7 @@
 class MaintenanceStaff < ApplicationRecord
   belongs_to :user
 
-  enum city: { ormoc_city: 0, cebu_city: 1, Ormoc_and_Cebu: 2 }
+  enum city: { ormoc_city: 0, cebu_city: 1, ormoc_and_cebu: 2 }
 
   validates :city, presence: true
 
