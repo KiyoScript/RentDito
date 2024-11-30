38bbe605e2bcfce9c311590baf763d667bc1b0c4
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-11-30 18:16:36 +0800

    RD-Updated the Subject of the email for Charge Penalty

--- app/mailers/notification_penalty_mailer.rb
+++ app/mailers/notification_penalty_mailer.rb
@@ -3,6 +3,6 @@ class NotificationPenaltyMailer < ApplicationMailer
     @user = user
     @billing = billing
     @charge = charge
-    mail(from: email_sender, to: email_to_deliver(user.email), subject: "Penalty Zone this #{Date.current.strftime("%B %d, %Y")}")
+    mail(from: email_sender, to: email_to_deliver(user.email), subject: "Penalty Zone for #{@billing.billing_type == 'wifi' ? 'Wi-Fi and Rental' : @billing.billing_type.titleize } Bill this #{Date.current.strftime("%B %d, %Y")}")
   end
 end
