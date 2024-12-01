9573e908aee74ff0080d3f1d0b42a2921a3b178f
DanTech2002 <danielligutan123@gmail.com>
2024-12-01 17:59:08 +0800

    RD-Added Dark Mode to Toastr

--- app/javascript/controllers/flash_messages_controller.js
+++ app/javascript/controllers/flash_messages_controller.js
@@ -3,6 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash-messages"
 export default class extends Controller {
   connect() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+
+    const isDarkTheme = hours >= 20 || hours < 5;
+
     const noticeMessage = this.element.dataset.notice;
     const alertMessage = this.element.dataset.alert;
 
@@ -46,12 +51,34 @@ export default class extends Controller {
 
     if (noticeMessage) {
       const formattedNotice = processMessage(noticeMessage);
-      toastr.success(formattedNotice);
+      if (isDarkTheme){
+        const style = document.createElement('style');
+        style.innerHTML = `
+          .toast-success {
+            background-color: #2b2c40 !important;
+          }
+        `;
+        document.head.appendChild(style);
+        toastr.success(formattedNotice);
+      } else {
+        toastr.success(formattedNotice);
+      }
     }
 
     if (alertMessage) {
       const formattedAlert = processMessage(alertMessage);
-      toastr.error(formattedAlert);
+      if (isDarkTheme){
+        const style = document.createElement('style');
+        style.innerHTML = `
+          .toast-error {
+            background-color: #2b2c40 !important;
+          }
+        `;
+        document.head.appendChild(style);
+        toastr.error(formattedAlert);
+      } else {
+        toastr.error(formattedAlert);
+      }
     }
   }
 }
