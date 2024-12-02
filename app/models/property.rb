ac69a03afd68abcc391185cb3d9dcdb97fa7aa4c
DanTech2002 <danielligutan123@gmail.com>
2024-12-02 13:10:17 +0800

    RD-HotFix Bug on Caretaker Notification v3

--- app/models/property.rb
+++ app/models/property.rb
@@ -30,7 +30,7 @@ class Property < ApplicationRecord
   end
 
   def active_tenants
-    active_tenants = tenants.includes(:user).where(users: { status: ["verified", "unverified", "incomplete"] })
+    active_tenants = tenants.includes(:user).where(users: { status: ["verified", "unverified", "incomplete"], role: "tenant" })
     (active_tenants).uniq
   end
 end
